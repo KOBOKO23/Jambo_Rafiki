b6d4d9dd9694195ea467cb2f23b3cea3d73eabc3
KOBOKOMASERO <kobokophilip@gmail.com>
2026-04-28 13:02:48 +0300

    refactor goodle url

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -3,6 +3,5 @@ export const CONTACT_INFO = {
   contactPerson: 'Benjamin Oyoo Ondoro',
   postalAddressLines: ['P.O Box 311 – 40222', 'OYUGIS - KENYA'],
   locationLabel: 'Oyugis, Kenya',
-  mapsEmbedUrl: 'https://maps.google.com/maps?q=Oyugis%2C%20Kenya&output=embed',
-  facebookUrl: 'https://www.facebook.com/share/18WHSiAAkr/',
+  mapsEmbedUrl: 'https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Oyugis,Kenya',  facebookUrl: 'https://www.facebook.com/share/18WHSiAAkr/',
 } as const;
