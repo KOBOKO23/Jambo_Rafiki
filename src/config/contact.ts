575afba85641003a9643c72dc021ff7ea4264acb
KOBOKOMASERO <kobokophilip@gmail.com>
2026-04-28 15:48:01 +0300

    fix CSP for Google Maps iframe

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -3,5 +3,5 @@ export const CONTACT_INFO = {
   contactPerson: 'Benjamin Oyoo Ondoro',
   postalAddressLines: ['P.O Box 311 – 40222', 'OYUGIS - KENYA'],
   locationLabel: 'Oyugis, Kenya',
-  mapsEmbedUrl: 'https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Oyugis,Kenya',  facebookUrl: 'https://www.facebook.com/share/18WHSiAAkr/',
+  mapsEmbedUrl: 'https://www.google.com/maps?q=Oyugis,Kenya&output=embed',
 } as const;
